c781695270f9eae7f27c0f737c0a0a83fffd3563
singleDeveloper63 <singleDeveloper63@gmail.com>
2020-05-26 19:12:40 +0300

    Changed

---
+++ src/functions/_inherit.js
@@ -0,0 +1,8 @@
+function _inherits(SubClass, SuperClass) {
+	if (typeof SuperClass !== "function" && SuperClass !== null) {
+		throw new TypeError("Super expression must either be null or a function, not " + typeof SuperClass);
+	}
+	SubClass.prototype = new SuperClass();
+}
+
+export default _inherits;
