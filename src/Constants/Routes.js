fc826ce006ccaf44db51a24c38f8e5c708a5250a
singleDeveloper63 <singleDeveloper63@gmail.com>
2020-05-26 19:12:40 +0300

    Changed

---
+++ src/Constants/Routes.js
@@ -0,0 +1,13 @@
+const Routes = {
+    HOME : '/',
+    IT_NEWS : '/it-news',
+    PROLANGS : '/prolangs',
+    ALGORITHMS : '/algorithms',
+    RESOURCES : '/resources',
+    ACCOUNT : '/account',
+    LOGIN : '/login',
+    REGISTER : '/register',
+    POST : '/post'
+}
+
+export default Routes;
